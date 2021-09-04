7f9c595d197458df7ac3a9339d3372bcc8c57d25
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-09-04 11:20:06 +0600

    install defendancy and setup folder structure

---
+++ src/components/categoriesBar/CategoriesBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './_categoriesBar.scss';
+
+const CategoriesBar = () => {
+    return (
+        <div>
+            categoriesBar
+        </div>
+    )
+}
+export default CategoriesBar;
---
+++ src/components/video/Video.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './_video.scss';
+
+const Video = () => {
+    return (
+        <div>
+            video 
+        </div>
+    )
+}
+export default Video;
